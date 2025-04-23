2c6dc546f671113373f28e6ce186bb8e169c0603
Aaron Marshall <30453252+msmc19@users.noreply.github.com>
2025-04-22 23:10:15 -0400

    Update Expertise.tsx

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDatabase, faChartLine, faCogs } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase, faChartLine, faCogs } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
