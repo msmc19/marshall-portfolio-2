959a5e4ad30f3a747f166d76551c76325debc598
Aaron Marshall <30453252+msmc19@users.noreply.github.com>
2025-04-22 23:37:03 -0400

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,51 +16,38 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
+            date="June 2020 - August 2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title">Shift Lead</h3>
+            <h4 className="vertical-timeline-element-subtitle">Brentwood, TN; Nashville, TN; Knoxville, TN</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              * Coordinated and overhauled maintenance projects to ensure seamless appliance and kitchen operations
+              * Streamlined store management through opening and closing tasks, including inventory logging, preparation, and cleaning
+              * Worked among multiple franchise locations exemplifying cultural adaptability (Cool Springs, Green Hills, Turkey Creek)
+              * Garnered knowledge of basic cooking techniques to execute tasks efficiently such as sauteing, frying, baking, and grilling meats and vegetables
+              * Trained between 10-20 newly hired line and front workers to integrate smoothly into operations
+              * Led operations under high-pressure, serving 50-150 customers while maintaining a focus on quality and
+              consistency
+              * Managed front and back of house staff during peak sales periods, handling $3,000 - $5,000 transactions between 4-7 hour shifts
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="June 2023 - July 2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title">Cashier / Merchandiser</h3>
+            <h4 className="vertical-timeline-element-subtitle">Brentwood, TN</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              * Monitored sales performance ranging from $10,000 to $20,000 and budgeted merchandise displays accordingly
+              * Reinforced compliance with all local regulations (TABC) governing product sales standards
+              * Operated hand trucks and pallet jacks for maneuvering products throughout the store
+              * Managed daily stocking and display of merchandise valued at upwards of $2,000
             </p>
           </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 - 2021"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
-            <p>
-              Full-stack Development, API Development, User Experience
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2020"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
-            <p>
-              Automation, Data Governance, Statistical Analysis
-            </p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
