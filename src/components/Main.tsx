9352c580a9dd37a9645f6e18629629335383d0dc
Aaron Marshall <30453252+msmc19@users.noreply.github.com>
2025-04-22 21:34:59 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22Aaron.JPG%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-04-22T01%3A28%3A40.576Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F6de4b3a015a14527%2FAaron.JPG%3FExpires%3D1839979721%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DWQ2DQxP-mrKkvi3Xifyz5Kn41NFBZcknAQMD1NjBt4iZyB10tcHuo2oFVwJGMoQN~DWbxZdvrKKkmfObHmD1CsIImUl--Ger~NMY7NkI3pd-QF~Z1KwB6Qc4QB0YGqcraycUOMwITE9GVEcuCXTVG2RRGgtjbX0blqxRR6H8JYijRYaa1fT3Wwi8pBjqZA6viPv6iQ~8OlDPA~bAVY6kXGp0iHFh~NxChBjC882tyjuwznDATUsL68Zf~GxTv8mYcRPaLANoI0~3xo39bviVxT9WA8ec0enVN08rcVZ3OLZgJwiw0vdEVVJNn~lw~z2Vo1hyPvWDSB7NDy0kxp-Pzw__%22%7D" alt="Avatar" />
+          <img src="https://ibb.co/5gbPJ8Wk" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
